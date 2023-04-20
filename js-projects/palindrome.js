262bb04b49072126f1f844694c0d5d25c52147ed
dawidlehai <haidawidle@gmail.com>
2023-04-20 13:53:06 +0200

    feat: add first working version of the palindrome function

--- js-projects/palindrome.js
+++ js-projects/palindrome.js
@@ -1,4 +1,10 @@
 function palindrome(str) {
+  const regex = /[^a-z0-9]/gi;
+  const strClean = str.replace(regex, "").toLowerCase();
+
+  for (let i = 0; i < strClean.length; i++)
+    if (strClean[i] !== strClean[strClean.length - i - 1]) return false;
+
   return true;
 }
 
