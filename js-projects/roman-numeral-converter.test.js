60b3436ea92d8741aa445392c0ea0c13dd60b466
dawidlehai <haidawidle@gmail.com>
2023-04-22 12:17:14 +0200

    test: make convertToRoman function tests clearer

--- js-projects/roman-numeral-converter.test.js
+++ js-projects/roman-numeral-converter.test.js
@@ -1,28 +1,107 @@
 const convertToRoman = require("./roman-numeral-converter");
 
-test("2 -> II", () => expect(convertToRoman(2)).toBe("II"));
-test("3 -> III", () => expect(convertToRoman(3)).toBe("III"));
-test("4 -> IV", () => expect(convertToRoman(4)).toBe("IV"));
-test("5 -> V", () => expect(convertToRoman(5)).toBe("V"));
-test("9 -> IX", () => expect(convertToRoman(9)).toBe("IX"));
-test("12 -> XII", () => expect(convertToRoman(12)).toBe("XII"));
-test("16 -> XVI", () => expect(convertToRoman(16)).toBe("XVI"));
-test("29 -> XXIX", () => expect(convertToRoman(29)).toBe("XXIX"));
-test("44 -> XLIV", () => expect(convertToRoman(44)).toBe("XLIV"));
-test("45 -> XLV", () => expect(convertToRoman(45)).toBe("XLV"));
-test("68 -> LXVIII", () => expect(convertToRoman(68)).toBe("LXVIII"));
-test("83 -> LXXXIII", () => expect(convertToRoman(83)).toBe("LXXXIII"));
-test("97 -> XCVII", () => expect(convertToRoman(97)).toBe("XCVII"));
-test("99 -> XXIX", () => expect(convertToRoman(99)).toBe("XXIX"));
-test("400 -> CD", () => expect(convertToRoman(400)).toBe("CD"));
-test("500 -> D", () => expect(convertToRoman(500)).toBe("D"));
-test("501 -> DI", () => expect(convertToRoman(501)).toBe("DI"));
-test("649 -> DCXLIX", () => expect(convertToRoman(649)).toBe("DCXLIX"));
-test("798 -> DCCXCVIII", () => expect(convertToRoman(798)).toBe("DCCXCVIII"));
-test("891 -> DCCCXCI", () => expect(convertToRoman(891)).toBe("DCCCXCI"));
-test("1000 -> M", () => expect(convertToRoman(1000)).toBe("M"));
-test("1004 -> MIV", () => expect(convertToRoman(1004)).toBe("MIV"));
-test("1006 -> MVI", () => expect(convertToRoman(1006)).toBe("MVI"));
-test("1023 -> MXXIII", () => expect(convertToRoman(1023)).toBe("MXXIII"));
-test("2014 -> MMXIV", () => expect(convertToRoman(2014)).toBe("MMXIV"));
-test("3999 -> MMMCMXCIX", () => expect(convertToRoman(3999)).toBe("MMMCMXCIX"));
+describe("convertToRoman function", () => {
+  test("should return II when given 2", () => {
+    expect(convertToRoman(2)).toBe("II");
+  });
+
+  test("should return III when given 3", () => {
+    expect(convertToRoman(3)).toBe("III");
+  });
+
+  test("should return IV when given 4", () => {
+    expect(convertToRoman(4)).toBe("IV");
+  });
+
+  test("should return V when given 5", () => {
+    expect(convertToRoman(5)).toBe("V");
+  });
+
+  test("should return IX when given 9", () => {
+    expect(convertToRoman(9)).toBe("IX");
+  });
+
+  test("should return XII when given 12", () => {
+    expect(convertToRoman(12)).toBe("XII");
+  });
+
+  test("should return XVI when given 16", () => {
+    expect(convertToRoman(16)).toBe("XVI");
+  });
+
+  test("should return XXIX when given 29", () => {
+    expect(convertToRoman(29)).toBe("XXIX");
+  });
+
+  test("should return XLIV when given 44", () => {
+    expect(convertToRoman(44)).toBe("XLIV");
+  });
+
+  test("should return XLV when given 45", () => {
+    expect(convertToRoman(45)).toBe("XLV");
+  });
+
+  test("should return LXVIII when given 68", () => {
+    expect(convertToRoman(68)).toBe("LXVIII");
+  });
+
+  test("should return LXXXIII when given 83", () => {
+    expect(convertToRoman(83)).toBe("LXXXIII");
+  });
+
+  test("should return XCVII when given 97", () => {
+    expect(convertToRoman(97)).toBe("XCVII");
+  });
+
+  test("should return XXIX when given 99", () => {
+    expect(convertToRoman(99)).toBe("XXIX");
+  });
+
+  test("should return CD when given 400", () => {
+    expect(convertToRoman(400)).toBe("CD");
+  });
+
+  test("should return D when given 500", () => {
+    expect(convertToRoman(500)).toBe("D");
+  });
+
+  test("should return DI when given 501", () => {
+    expect(convertToRoman(501)).toBe("DI");
+  });
+
+  test("should return DCXLIX when given 649", () => {
+    expect(convertToRoman(649)).toBe("DCXLIX");
+  });
+
+  test("should return DCCXCVIII when given 798", () => {
+    expect(convertToRoman(798)).toBe("DCCXCVIII");
+  });
+
+  test("should return DCCCXCI when given 891", () => {
+    expect(convertToRoman(891)).toBe("DCCCXCI");
+  });
+
+  test("should return M when given 1000", () => {
+    expect(convertToRoman(1000)).toBe("M");
+  });
+
+  test("should return MIV when given 1004", () => {
+    expect(convertToRoman(1004)).toBe("MIV");
+  });
+
+  test("should return MVI when given 1006", () => {
+    expect(convertToRoman(1006)).toBe("MVI");
+  });
+
+  test("should return MXXIII when given 1023", () => {
+    expect(convertToRoman(1023)).toBe("MXXIII");
+  });
+
+  test("should return MMXIV when given 2014", () => {
+    expect(convertToRoman(2014)).toBe("MMXIV");
+  });
+
+  test("should return MMMCMXCIX when given 3999", () => {
+    expect(convertToRoman(3999)).toBe("MMMCMXCIX");
+  });
+});
