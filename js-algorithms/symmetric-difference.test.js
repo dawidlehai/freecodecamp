ec6de9519fc1c7b02f0e413f2494e1647b97bcd9
dawidlehai <haidawidle@gmail.com>
2023-04-24 17:06:43 +0200

    test: add tests for symDif function

---
+++ js-algorithms/symmetric-difference.test.js
@@ -0,0 +1,42 @@
+const symDif = require("./symmetric-difference");
+
+describe("symDif function", () => {
+  test("should return [3, 4, 5] when given [1, 2, 3], [5, 2, 1, 4]", () => {
+    expect(symDif([1, 2, 3], [5, 2, 1, 4])).toBe([3, 4, 5]);
+  });
+
+  test("should return [3, 4, 5] when given [1, 2, 3, 3], [5, 2, 1, 4]", () => {
+    expect(symDif([1, 2, 3, 3], [5, 2, 1, 4])).toBe([3, 4, 5]);
+  });
+
+  test("should return [3, 4, 5] when given [1, 2, 3], [5, 2, 1, 4, 5]", () => {
+    expect(symDif([1, 2, 3], [5, 2, 1, 4, 5])).toBe([3, 4, 5]);
+  });
+
+  test("should return [1, 4, 5] when given [1, 2, 5], [2, 3, 5], [3, 4, 5]", () => {
+    expect(symDif([1, 2, 5], [2, 3, 5], [3, 4, 5])).toBe([1, 4, 5]);
+  });
+
+  test("should return [1, 4, 5] when given [1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]", () => {
+    expect(symDif([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5])).toBe([1, 4, 5]);
+  });
+
+  test("should return [2, 3, 4, 6, 7] when given [3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]", () => {
+    expect(symDif([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3])).toBe(
+      [2, 3, 4, 6, 7]
+    );
+  });
+
+  test("should return [1, 2, 4, 5, 6, 7, 8, 9] when given [3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]", () => {
+    expect(
+      symDif(
+        [3, 3, 3, 2, 5],
+        [2, 1, 5, 7],
+        [3, 4, 6, 6],
+        [1, 2, 3],
+        [5, 3, 9, 8],
+        [1]
+      )
+    ).toBe([1, 2, 4, 5, 6, 7, 8, 9]);
+  });
+});
